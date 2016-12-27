d21a41b0d2074032ab6f97cf6a7f9624cc2c6b23
1162643@qq.com <1162643@qq.com>
2016-12-27 18:08:24 +0800

    fixed the length of the number

--- index.js
+++ index.js
@@ -1,58 +1,66 @@
 /**
  * Created by Simon on 16/12/27.
  */
-angular.module("timeApp", []).controller("timeCtrl", ["$scope", "$interval", function ($scope, $interval) {
+angular.module("timeApp", []).controller("timeCtrl", ["$scope", "$interval", "timeService", function ($scope, $interval, timeService) {
     $scope.edit = true;
     $scope.go = true;
+    $scope.timeCtrl={
+        hour:"00",
+        min:"00",
+        sec:"10"
+    }
     $scope.hour = "00";
-    $scope.min = "00";
-    $scope.sec = "00";
+    $scope.min = "01";
+    $scope.sec = "11";
     $scope.start = function () {
-        if($scope.hour == 0 && $scope.min == 0 && $scope.sec == 0){
+        if ($scope.timeCtrl.hour == 0 && $scope.timeCtrl.min == 0 && $scope.timeCtrl.sec == 0) {
             alert("Please type the number into the input!")
-        }else{
+        } else {
             $scope.go = false;
             $scope.edit = true;
-            checkNull($scope.hour);
-            checkNull($scope.min);
-            checkNull($scope.sec);
-            t = $interval(startTime, 1000);
-        }
-        function startTime() {
-            $scope.sec--;
-
-            if ($scope.sec < 0) {
-                $scope.sec = 59;
-                $scope.min = $scope.min - 1
-            }
-            if ($scope.min < 0) {
-                $scope.min = 59;
-                $scope.hour = $scope.hour - 1;
-            }
-            checktime($scope.hour);
-            checktime($scope.min);
-            checktime($scope.sec);
-            if ($scope.hour == 0 && $scope.min == 0 && $scope.sec == 0) {
-                $interval.cancel(t);
-            }
-        }
-        function checktime(param) {
-            if (param < 10) {
-                param = "0" + param;
-            }
-            return param;
+            $scope.t = $interval(function(){
+                timeService.startTime($scope.timeCtrl)
+                if ($scope.timeCtrl.hour == 0 && $scope.timeCtrl.min == 0 && $scope.timeCtrl.sec == 0) {
+                    $interval.cancel($scope.t);
+                    $scope.go = true;
+                }
+            }, 1000);
         }
 
-        function checkNull(param) {
-            if (param == "") {
-                param = "00";
-            }
-            return param;
-
-        }
     };
     $scope.stop = function () {
         $scope.go = true;
-        $interval.cancel(t);
+        $interval.cancel($scope.t);
     }
 }])
+
+    .service("timeService", function () {
+        return {
+            startTime: function (timeCtrl) {
+
+                timeCtrl.sec--;
+                if (timeCtrl.sec < 0) {
+                    timeCtrl.sec = 59;
+                    timeCtrl.min = timeCtrl.min - 1;
+                }
+
+                if (timeCtrl.min < 0) {
+                    timeCtrl.min = 59;
+                    timeCtrl.hour = timeCtrl.hour - 1;
+                }
+
+
+                timeCtrl.hour=this.checkTime(timeCtrl.hour);
+                timeCtrl.min=this.checkTime(timeCtrl.min);
+                timeCtrl.sec=this.checkTime(timeCtrl.sec);
+
+            },
+            checkTime: function (param) {
+                var string = param.toString();
+                if (string.length === 1) {
+                    string = "0" + string;
+                }
+                return string;
+            }
+        }
+    })
