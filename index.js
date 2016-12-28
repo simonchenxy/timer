f0a60ed888fe7d55a579101dffe6b7dc22254b06
1162643@qq.com <1162643@qq.com>
2016-12-28 10:45:44 +0800

    Ordinary modify

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * Created by Simon on 16/12/27.
+ * Created by Simon on 16/12/28.
  */
 angular.module("timeApp", [])
     .controller("timeCtrl", ["$scope", "$interval", "timeService", function ($scope, $interval, timeService) {
@@ -11,6 +11,7 @@ angular.module("timeApp", [])
             sec: "00"
         };
         $scope.start = function () {
+            timeService.inject($scope.timeCtrl);
             if ($scope.timeCtrl.hour == 0 && $scope.timeCtrl.min == 0 && $scope.timeCtrl.sec == 0) {
                 alert("Please type the number into the input!")
             } else {
@@ -21,6 +22,7 @@ angular.module("timeApp", [])
                     if ($scope.timeCtrl.hour == 0 && $scope.timeCtrl.min == 0 && $scope.timeCtrl.sec == 0) {
                         $interval.cancel($scope.t);
                         $scope.go = true;
+                        $scope.alert = true;
                     }
                 }, 1000);
             }
@@ -28,7 +30,20 @@ angular.module("timeApp", [])
         $scope.stop = function () {
             $scope.go = true;
             $interval.cancel($scope.t);
+        };
+        $scope.reset = function () {
+            $scope.timeCtrl = {
+                hour: "00",
+                min: "00",
+                sec: "00"
+            };
+            $interval.cancel($scope.t);
+            $scope.go = true;
+        };
+        $scope.close = function () {
+            $scope.alert = false;
         }
+
     }])
     .service("timeService", function () {
         return {
@@ -42,9 +57,7 @@ angular.module("timeApp", [])
                     param.min = 59;
                     param.hour = param.hour - 1;
                 }
-                param.hour = this.checkTime(param.hour);
-                param.min = this.checkTime(param.min);
-                param.sec = this.checkTime(param.sec);
+                this.inject(param);
             },
             checkTime: function (param) {
                 var string = param.toString();
@@ -52,6 +65,31 @@ angular.module("timeApp", [])
                     string = "0" + string;
                 }
                 return string;
+            },
+            checkNull: function (param) {
+                if (param == "") {
+                    param = "00";
+                }
+                return param;
+            },
+            inject:function(param){
+                param.hour = this.checkTime(param.hour);
+                param.min = this.checkTime(param.min);
+                param.sec = this.checkTime(param.sec);
+                param.hour = this.checkNull(param.hour);
+                param.min = this.checkNull(param.min);
+                param.sec = this.checkNull(param.sec);
             }
+        }
+    })
+    .directive("alert", function () {
+        return {
+            restrict: "C",
+            replace:false,
+            template:
+            "<div>" +
+            "<span ng-click='close()'>X</span>" +
+            "<p>Done!</p>" +
+            "</div>"
         }
     })
